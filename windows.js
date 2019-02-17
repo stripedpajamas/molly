d46b0cf47cad997d1250abd974f00cd276abdfa0
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-17 13:49:03 -0500

    Reorder commands

--- windows.js
+++ windows.js
@@ -2,11 +2,11 @@ const exec = require('child_process').execSync
 
 // to run the scheduled task creation
 module.exports = ({ username, password, taskUsername, taskPassword }) => {
-  const a = `$user = '${taskUsername}'`
-  const b = `$pass = '${taskPassword}'`
-  const c = `$sta = New-ScheduledTaskAction -Execute 'C:\\QVDNS\\Molly -q -u ${username} -p ${password}'`
-  const d = '$sts = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries'
-  const e = '$stt = New-ScheduledTaskTrigger -Once -At $(Get-Date) -RepetitionInterval $(New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)'
+  const a = '$stt = New-ScheduledTaskTrigger -Once -At $(Get-Date) -RepetitionInterval $(New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)'
+  const b = '$sts = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries'
+  const c = `$user = '${taskUsername}'`
+  const d = `$pass = '${taskPassword}'`
+  const e = `$sta = New-ScheduledTaskAction -Execute 'C:\\QVDNS\\Molly -q -u ${username} -p ${password}'`
   const f = '$task = New-ScheduledTask -Action $sta -Trigger $stt -Settings $sts'
   const g = "$task | Register-ScheduledTask -TaskName 'QV DNS Update' -User $user -Password $pass | Format-Wide"
   const script = [a, b, c, d, e, f, g].join(';')
