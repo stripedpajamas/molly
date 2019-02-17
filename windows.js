72e0191d2724d3574cbf181e2446a29e49a04bf7
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-17 13:38:21 -0500

    Use powershell for Windows script creation

--- windows.js
+++ windows.js
@@ -2,12 +2,20 @@ const exec = require('child_process').execSync
 
 // to run the scheduled task creation
 module.exports = ({ username, password, taskUsername, taskPassword }) => {
+  const a = `$user = '${taskUsername}'`
+  const b = `$pass = '${taskPassword}'`
+  const c = `$sta = New-ScheduledTaskAction -Execute 'C:\\QVDNS\\Molly -q -u ${username} -p ${password}'`
+  const d = '$sts = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries'
+  const e = '$stt = New-ScheduledTaskTrigger -Once -At $(Get-Date) -RepetitionInterval $(New-TimeSpan -Minutes 5) -RepetitionDuration ([TimeSpan]::MaxValue)'
+  const f = '$task = New-ScheduledTask -Action $sta -Trigger $stt -Settings $sts'
+  const g = "$task | Register-ScheduledTask -TaskName 'QV DNS Update' -User $user -Password $pass | Format-Wide"
+  const script = [a, b, c, d, e, f, g].join(';')
+  const cmd = `powershell -Command "& {${script}}"`
   let output = ''
-  const cmd = `schtasks /Create /tn "QV DNS Update" /sc MINUTE /mo 5 /ru "${taskUsername}" /rp "${taskPassword}" /tr "C:\\QVDNS\\Molly -q -u ${username} -p ${password}"`
   try {
     output = exec(cmd)
   } catch (e) {
     return false
   }
-  return output.includes('SUCCESS')
+  return output.includes('QV DNS Update')
 }
